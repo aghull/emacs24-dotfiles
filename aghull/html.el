83961f64eb62a6e2fb10743e03e8752cf7c666ae
Andrew Hull <aghull@gmail.com>
2015-03-23 17:14:25 -0400

    flycheck

---
+++ aghull/html.el
@@ -0,0 +1,13 @@
+(starter-kit-load "js")
+
+(require 'flycheck)
+(flycheck-define-checker jsxhint-checker
+  "A JSX syntax and style checker based on JSXHint."
+
+  :command ("jsxhint" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  :modes (jsx-mode))
+(add-hook 'jsx-mode-hook (lambda ()
+                          (flycheck-select-checker 'jsxhint-checker)
+                          (flycheck-mode)))
