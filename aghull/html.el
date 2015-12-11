7df2caf9b555a404339aafe76293e49846833632
Andrew Hull <aghull@gmail.com>
2015-12-11 11:20:16 -0500

    start removing start-kit

--- aghull/html.el
+++ aghull/html.el
@@ -1,5 +1,3 @@
-(starter-kit-load "js")
-
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
@@ -12,8 +10,12 @@
 
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode)) - hangs???
-(add-to-list 'auto-mode-alist '(".html" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.json5?\\'" . js-mode))
 
 (add-hook 'web-mode-hook
           (lambda () (modify-syntax-entry ?_ "_")))
+
+(font-lock-add-keywords 'web-mode
+                        '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
+                           1 font-lock-warning-face t)))
--- aghull/rails.el
+++ aghull/rails.el
@@ -1,5 +1,3 @@
-(starter-kit-load "ruby")
-
 (require 'rvm)
 (rvm-use-default)
 
@@ -8,11 +6,20 @@
 (add-to-list 'auto-mode-alist '("\\.rhtml\\'" . rhtml-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
 
-;; restore transpose-lines key
+(add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+
+(add-to-list 'completion-ignored-extensions ".rbc")
+
 (eval-after-load 'ruby-mode
   '(progn
-     (define-key ruby-mode-map (kbd "C-x C-t") 'transpose-lines)
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+     (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
+     (define-key ruby-mode-map (kbd "C-c l") "lambda")
+     (define-key ruby-mode-map (kbd "C-x C-t") 'transpose-lines) ;; restore transpose-lines key
      (define-key ruby-mode-map [\C-\s-left] 'ruby-beginning-of-block)
      (define-key ruby-mode-map [\C-\s-right] 'ruby-end-of-block)))
