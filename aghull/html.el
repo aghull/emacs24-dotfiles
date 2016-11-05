7965d82587748b1bf7e3e0f7de135bed977f9646
Andrew Hull <aghull@gmail.com>
2016-11-05 18:24:50 -0400

    jsx handling

--- aghull/html.el
+++ aghull/html.el
@@ -3,6 +3,7 @@
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-script-padding 2)
 (setq web-mode-enable-auto-quoting nil)
+(setq web-mode-attr-indent-offset 2)
 
 (require 'flycheck)
 (require 'rainbow-mode)
@@ -19,9 +20,16 @@
 ;; (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.less\\'" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\(on\\)\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\(on\\)?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.json5?\\'" . js-mode))
+
+(setq web-mode-engines-alist '(("jsx" . "\\.js\\'")))
+(setq web-mode-content-types '(("css" . "\\.\\(s?css\\|css\\.erb\\)\\'")
+                               ("json" . "\\.\\(api\\|json\\|jsonld\\)\\'")
+                               ("jsx" . "\\.[jt]s\\(x\\)?\\'")
+                               ("xml" . "\\.xml\\'")
+                               ("html" . ".")))
 
 (add-hook 'web-mode-hook
           (lambda () (modify-syntax-entry ?_ "_")))
